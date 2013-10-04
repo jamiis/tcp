88cbaded3a764799d3a77822f9119cf2a2258e50
Jamis Johnson <jamismanwaring@gmail.com>
2013-10-04 10:59:53 -0600

    add broadcast functionality
    cleans up and sets the standard for communication between to server an
    client to be json.
    also added connections and allowed dicts to hold allowed users (which
    will eventually be read in from a file) and current connections with the
    server.

--- client.py
+++ client.py
@@ -1,7 +1,68 @@
-import argparse, ipaddress, socket, sys
+import argparse, socket, sys, json, thread
+
+BUFFER_SIZE = 1024
+
+def blocked():
+    print "You have been blocked from logging in for 60 seconds"
+
+def connect(sock, host, port):
+    sock.connect((host, port))
+
+    # first check if machine is blocked 
+    sock.send("is_blocked")
+    access = json.loads(sock.recv(BUFFER_SIZE))
+    if access['is_blocked']:
+        blocked()
+        return
+
+    # submit credentials for access
+    while not access['is_granted']:
+        user = raw_input("Username: ")
+        pwd  = raw_input("Password: ")
+        sock.sendall(json.dumps({
+            'user': user,
+            'pwd' : pwd,
+        }))
+        access = json.loads(sock.recv(BUFFER_SIZE))
+        if not access['is_granted']:
+            print "access denied. bad username or password"
+            continue
+        if access['is_blocked']:
+            blocked()
+            return
+
+    print "access granted. welcome."
+
+    # spawn thread to handle receiving all server communication
+    # thread required bc server can broadcast to client w/out client talking to server
+    thread.start_new_thread(recv_thread, (sock,))
+
+    while True:
+        # prompt user for input
+        prompt = raw_input().strip()
+        if not prompt:
+            continue # there wasn't any cmd input
+
+        # extract command and args from user input
+        split = prompt.split(' ',1)
+        request = { 'cmd': split[0] }
+        if len(split) > 1:
+            request['args'] = split[1]
+
+        # send command + args to server
+        sock.sendall(json.dumps(request))
+
+def recv_thread(sock):
+    while True:
+        # when socket is closed, destroy thread
+        if not sock: return
+        # await comm. from server
+        response = json.loads(sock.recv(BUFFER_SIZE))
+        if 'echo' in response:
+            print response['echo']
+
 
 if __name__ == '__main__':
-    print 'creating client socket'
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'host', 
@@ -14,3 +75,18 @@ if __name__ == '__main__':
     )
     # TODO validate host and port
 
+    args = parser.parse_args()
+
+    # conditionally resolve localhost
+    if args.host == "localhost":
+        host = socket.gethostbyname(args.host) 
+    else:
+        host = args.host 
+
+    # instatiate socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "attempting to connect on {0}:{1}".format(host, args.port)
+    connect(sock, host, args.port)
+
+    # when connect returns, close socket
+    sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,24 @@
-import argparse, socket, sys, pprint
-from thread import start_new_thread
+import argparse, sys, socket, json, thread
+
+BUFFER_SIZE = 1024
+
+def whoelse(cport, request):
+    print 'whoelse!!!'
+
+def wholasthr(cport, request):
+    print 'wholasthr!!!'
+
+def broadcast(cport, request):
+    # notify user if no broadcast msg was included
+    if 'args' not in request or request['args'] == "":
+        response = { "echo": "please specify a message you would like to broadcast" }
+        connection = connections[cport]['conn']
+        connection.sendall(json.dumps(response))
+    # broadcast message to all connections
+    response = { "echo": "<broadcast>: {0}".format(request['args']) }
+    for port, sock_info in connections.iteritems():
+        print port, sock_info, response
+        sock_info['conn'].sendall(json.dumps(response))
 
 commands = {
     'whoelse'  : whoelse,
@@ -7,31 +26,111 @@ commands = {
     'broadcast': broadcast,
 }
 
-def client_thread(c_socket, addr):
-    while True:
-        received = c_socket.recv(1024).strip()
+# keep track of all clients connected
+# dict keys will be client port numbers
+connections = {}
 
-        # extract cmd & args, validate, call corresponding fcn in commands
-        parsed = received.split(' ',1)
-        cmd = parsed[0]
-        args = ''
-        if len(parsed) > 1:
-            args = parsed[1] 
+# the usernames and passwords of those allowed to login
+# (*obviously* unsecure)
+# TODO must be a file
+allowed = {
+    'Columbia' : '116bway',
+    'SEAS'     : 'summerisover',
+    'csee4119' : 'lotsofexams',
+    'foobar'   : 'passpass',
+    'windows'  : 'withglass',
+    'Google'   : 'hasglasses',
+    'facebook' : 'wastingtime',
+    'wikipedia': 'donation',
+    'network'  : 'seemse',
+}
 
-        if cmd not in commands:
-            c_socket.sendall("The command '{0}' is not supported".format(cmd))
-        else:
-            commands[cmd]
+def block_machine(conn, addr):
+    ''' blocks logins from addr for 60 seconds and closes the connection '''
+    print "blocking connections from {0} for 60 seconds".format(addr[0])
+    # TODO store machine addr and current time in list of blocked addresses
+    close_connection(conn, addr)
 
-        # close connection
-        print data
-        if not data:
-            break
+def is_blocked(conn, addr):
+    # TODO
+    return False
 
-        c_socket.sendall('Ok...' + data)
-
+def close_connection(conn, addr):
     print 'closing connection with: {0}:{1}'.format(addr[0], addr[1])
-    c_socket.close()
+    conn.close()
+
+def client_thread(conn, addr):
+    # client must gain access before allowed to submit cmds
+    access = { 
+        'is_granted': False,
+        'is_blocked': is_blocked(conn, addr),
+    }
+
+    # check right away if client is listed as blocked
+    data = conn.recv(BUFFER_SIZE)
+    if data != "is_blocked":
+        return # the client cheated, so return and close conn
+    conn.send(json.dumps(access))
+
+    # await submission of client credentials
+    while not access['is_granted']:
+        for attempt in [1,2,3]:
+            creds = json.loads(conn.recv(BUFFER_SIZE))
+
+            # verify client credentials
+            if creds['pwd'] == allowed.get(creds['user'], False):
+                # credentials passed, access granted
+                print "access granted to client {0}:{1}" \
+                    .format(addr[0],addr[1])
+                access['is_granted'] = True
+                conn.sendall(json.dumps(access))
+                break
+            else:
+                # client gave bad credentials
+                if attempt == 3:
+                    # block logins from this machine
+                    access['is_blocked'] = True
+                    conn.sendall(json.dumps(access))
+                    block_machine(conn, addr)
+                    return
+                else:
+                    # notify client access was denied
+                    print "access denied to client {0}:{1}"\
+                        .format(addr[0],addr[1])
+                    conn.sendall(json.dumps(access))
+
+    # access granted to client at this point
+    # the client port is a unique id for this connection
+    cport = addr[1]
+    # add conn info to conns dict
+    connections[cport] = {
+        'conn': conn, 
+        'addr': addr,
+        'time': 999, # TODO
+        # TODO: do we need to store usernames to disallow signing
+        #       in multiple times by unser one username?
+    }
+
+    while True:
+        request = json.loads(conn.recv(BUFFER_SIZE))
+
+        print "received request: {0} from {1}:{2}" \
+            .format(request, addr[0], addr[1])
+
+        # extract cmd, validate, call corresponding func
+        cmd = request['cmd']
+        if cmd == 'exit': 
+            break
+        else if cmd not in commands:
+            # notify client that this is not a valid cmd
+            response = { "echo": "command '{0}' is not supported".format(cmd) }
+            conn.sendall(json.dumps(response))
+        else:
+            # command is valid, so call into function
+            commands[cmd](cport, request)
+
+    if conn:
+        close_connection(conn, addr)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -44,24 +143,28 @@ if __name__ == '__main__':
 
     # TODO validate port
 
-    # establish listening socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # instatiate socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     print 'socket initialized'
 
     try:
-        s.bind(('localhost', args.port))
+        host = socket.gethostbyname(socket.gethostname())
+        sock.bind((host, args.port))
     except socket.error, msg:
         print "an error occured binding the server socket. \
                error code: {0}, msg:{1}".format(msg[0], msg[1])
         sys.exit()
 
-    s.listen(100)
-    print 'socket listening'
+    sock.listen(100)
+
+    print 'socket listening on {0}:{1}'.format(host, args.port)
     
-    while 1:
-        c_socket, addr = s.accept()
-        print 'connected with: {0}:{1}'.format(addr[0], addr[1])
+    while True:
+        conn, addr = sock.accept()
+        # spawn thread to handle connection
+        thread.start_new_thread(client_thread, (conn, addr))
 
-        start_new_thread(client_thread, (c_socket, addr))
-
-    s.close()
+    if sock:
+        print 'closing socket'
+        sock.close()
